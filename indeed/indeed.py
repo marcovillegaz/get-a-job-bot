a24c78c5faa4117829b98404629d4ca77b5ead19
Marco Asus Laptop <marcovillegas.g@gmail.com>
2024-05-20 09:21:08 -0400

    Now Indeed class works different

--- indeed/indeed.py
+++ indeed/indeed.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 # Selenium tools
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -7,67 +10,283 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
-
 # Modules
-from utils.indeed import sign_in
-from utils.indeed import search
+import indeed.info as info
+from indeed.utils import logon
 
 
 class Indeed(webdriver.Firefox):
-    def __init__(self, driver_path="Browser\geckodriver.exe"):
-        # create a new Firefox session
-        # service = Service(executable_path=r"Browser\geckodriver.exe")
+    def __init__(self, driver_path=info.BASE_URL, teardown=False):
+        # Create a new Firefox session
         self.driver_path = driver_path
+        self.teardown = teardown
+
+        self.EMAIL = info.EMAIL
+        self.PASSWORD = info.PASSWORD
+        self.JOB = info.JOB
+        self.PLACE = info.PLACE
+
+        self.sleep = 5
+
+        os.environ["PATH"] += self.driver_path
+
+        # With the super() function, you dont have to write driver any time.
+        # webdriver is the parent class and Indeed the child.
         super(Indeed, self).__init__()
 
-        # self.driver = webdriver.Firefox(service=service)
-
-        # self.info = info
-        # open indeed
-        # self.driver.get("https://secure.indeed.com/account/login")
-        # self.driver.implicitly_wait(5)
+        self.implicitly_wait(5)
+        # self.maximize_window()
 
         # Create headless chrome
         # options = Options()
         # options.headless = True
 
-    """ def login(self):
-        print("Logging in Indeed".center(80, "-"))
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.teardown:
+            time.sleep(5)
+            self.quit()
+
+    def land_webpage(self):
+        self.get("https://secure.indeed.com/account/login")
+
+    def enter_email(self):
+        """This method enter the email in the input box"""
+
+        # Check email page
         try:
-            # Email
-            sign_in.enter_email(self.driver, self.info)
-
-            try:
-                # Password
-                sign_in.enter_password(self.driver, self.info)
-                sign_in.verification(self.driver, self.info)
-            except:
-                # Access code
-                sign_in.access_code(self.driver, self.info)
-
-            # Go to main page
-            home_button = (
-                WebDriverWait(self.driver, 10)
-                .until(
-                    EC.presence_of_element_located((By.XPATH, '//*[@id="FindJobs"]'))
-                )
-                .click()
+            # Find email box element
+            email_element = self.find_element(
+                By.ID,
+                "ifl-InputFormField-ihl-useId-passport-webapp-1",
             )
-            print("Now you are in home page")
+            print("Enter email".center(50, "-"))
+            # write the email in box
+            email_element.send_keys(self.EMAIL)
+
+            # Click Continue button
+            button = self.find_element(
+                By.CSS_SELECTOR,
+                'button[data-tn-element="auth-page-email-submit-button"]',
+            ).click()
+
+            time.sleep(self.sleep)
+
+            # Sometime a captcha appear and you have to click de button again.
+            print("Cheking for captcha:")
+            try:
+                # Check captcha with an element of the same page, if it exists, there is a captcha.
+                flag_element = self.find_element(
+                    By.CSS_SELECTOR,
+                    'h1[data-tn-section="auth-page-header--enter-email"]',
+                )
+                print("flag text:", flag_element.text)
+                print("¡A Captcha has appear!")
+
+                time.sleep(self.sleep)
+
+                # CLick Continue button
+                print("Click button again")
+                button = self.find_element(
+                    By.CSS_SELECTOR,
+                    'button[type="submit"]',
+                ).click()
+
+            except:
+                print("No captcha has appeared")
 
         except:
-            print("There was an error during login process")
+            print("There is an error with email page")
 
-    def find_job(self):
-        print(f"Searching a job of '{self.info.job}' in '{self.info.place}'")
-        search.enter_job(self.driver, self.info)
-        search.enter_place(self.driver, self.info)
+    def enter_password(self):
+        """This function is to enter tour password in Indeed"""
 
-        time.sleep(2)
-        button = self.driver.find_element(
-            By.XPATH,
-            "/html/body/div/div[1]/div/span/div[4]/div[1]/div/div/div/div/form/div/div[2]/button",
-        ).click()
+        # Check password page
+        try:
+            flag_element = self.find_element(
+                By.CSS_SELECTOR,
+                'h1[data-tn-section="auth-page-header--login"]',
+            )
+            print("\nflag text:", flag_element.text)
+            print("Enter password".center(50, "-"))
 
-        search.extract_results(self.driver) """
+            # Write password in text box
+            password_element = self.find_element(
+                By.ID,
+                "ifl-InputFormField-ihl-useId-passport-webapp-1",
+            )
+            password_element.send_keys(self.PASSWORD)
+
+            time.sleep(2)
+
+            # CLick Continue button
+            button = self.find_element(
+                By.CSS_SELECTOR,
+                'button[data-tn-element="auth-page-sign-in-password-form-submit-button"]',
+            ).click()
+
+            time.sleep(self.sleep)
+
+            # Sometime a captcha appear and you have to write the password again.
+            print("Checking for captcha...")
+            try:
+                # Check captcha with an element of the same page, if it exists, there is a captcha.
+                flag_element = self.find_element(
+                    By.CSS_SELECTOR,
+                    'h1[data-tn-section="auth-page-header--login"]',
+                )
+                print("\tflag text:", flag_element.text)
+                print("\t¡A Captcha has appear! Writing password again.")
+
+                time.sleep(self.sleep)
+
+                # Write password in text box
+                password_element = self.find_element(
+                    By.ID,
+                    "ifl-InputFormField-ihl-useId-passport-webapp-1",
+                )
+                password_element.send_keys(self.PASSWORD)
+
+                # CLick Continue button
+                button = self.find_element(
+                    By.CSS_SELECTOR,
+                    'button[data-tn-element="auth-page-sign-in-password-form-submit-button"]',
+                ).click()
+
+            except:
+                print("No captcha has appeared")
+
+        except:
+            # print("\tYou are not in password page")
+            pass
+
+    def verification(self):
+        """This function is used when Indeed want you to login with a 2 step verification"""
+        # Check 2 step verification page
+        try:
+            flag_element = self.find_element(
+                By.ID,
+                "pageHeaderText",
+            )
+            print("\nflag text:", flag_element.text)
+            print("2 step verification".center(50, "-"))
+            print(f"A 6 digit code was send to {self.EMAIL}")
+
+            # Enter the code
+            code = input("Introduce the code: ")
+
+            # Write the code in text box
+            input_element = self.find_element(
+                By.ID,
+                "verification_input",
+            )
+            input_element.send_keys(code)
+
+            # Click Continue button
+            button = self.find_element(
+                By.ID,
+                "submit-code",
+            ).click()
+
+            time.sleep(self.sleep)
+
+            # Sometime a captcha appear and you have to write the password again.
+            print("Checking for captcha...")
+            try:
+                # Check captcha with an element of the same page, if it exists, there is a captcha.
+                flag_element = self.find_element(
+                    By.ID,
+                    "pageHeaderText",
+                )
+                print("flag text:", flag_element.text)
+                print("¡A Captcha has appear! Writing password again.")
+
+                # Write the code in text box
+                input_element = self.find_element(
+                    By.ID,
+                    "verification_input",
+                )
+                input_element.send_keys(code)
+
+                time.sleep(self.sleep)
+
+                # CLick Continue button
+                button = self.find_element(
+                    By.ID,
+                    "submit-code",
+                ).click()
+
+            except:
+                print("No captcha has appeared")
+        except:
+            pass
+
+    def access_code(self):
+        """This function is used when indeed want you to login with a 6 digit acess code"""
+
+        # Check acces code page
+        try:
+            flag_element = self.find_element(
+                By.CSS_SELECTOR,
+                'h1[data-testid="auth-page-otp-header"]',
+            )
+            print("Access code".center(50, "-"))
+            print("flag_text:", flag_element.text)
+            print(f"A 6 digit code was send to {self.EMAIL}")
+
+            # Introduce the code
+            code = input("Introduce the 6 digit code: ")
+
+            # Write the code in text box
+            input_element = self.find_element(
+                By.ID,
+                "passcode-input",
+            )
+            input_element.send_keys(code + Keys.ENTER)
+
+            # CLick Continue button
+            button = self.find_element(
+                By.CSS_SELECTOR,
+                'button[data-tn-element="otp-verify-login-submit-button"]',
+            ).click()
+
+        except:
+            # print("No 6 digit verification")
+            pass
+
+    def homepage(self):
+        """Go to home page"""
+        # Check configuration web page
+        try:
+            flag_element = self.find_element(
+                By.CSS_SELECTOR,
+                'h1[data-testid="settings-Title"]',
+            )
+            print("flag text: ", flag_element.text)
+            print("Homepage".center(50, "-"))
+
+            home_button = self.find_element(
+                By.ID,
+                "FindJobs",
+            ).click()
+
+            print("You are in home page")
+
+        except:
+            pass
+
+    def search_job(self):
+        """Search your desirable job by place"""
+        time.sleep(1)
+
+        # Get job field
+        job_element = self.find_element(By.ID, "text-input-what")
+        # Write job in text box
+        job_element.send_keys(self.JOB)
+
+        time.sleep(1)
+
+        # Get place field
+        place_element = self.find_element(By.ID, "text-input-where")
+
+        # Write place in text box
+        place_element.send_keys(self.PLACE)
